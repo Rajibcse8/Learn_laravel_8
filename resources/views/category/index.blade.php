60fdb102fa90f065bead889b002221b01c6afe5d
Rajibcse8 <rajibtalukder119@gmail.com>
2021-07-25 23:00:26 +0600

    18.1 input form added

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,7 +1,62 @@
 @extends('layouts.app')
 @section('main_content')
- 
-  
+
+
+
+    <div class="py-12">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">
+                            All-Category
+                        </div>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">SL NO</th>
+                                    <th scope="col">Category Name</th>
+                                    <th scope="col">Created By</th>
+                                    <th scope="col">Created At</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            Add-Category
+                        </div>
+                        <div class="card-body">
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="category_name">Category Name</label>
+                                    <input type="text" class="form-control" name="category_name" id="category_name">
+                                </div>
+                                
+                                <button type="submit" class="btn btn-primary">Add category</button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+    </div>
+
+    {{--  --}}
+
 
 
 @endsection
