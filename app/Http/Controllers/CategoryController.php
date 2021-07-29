fd5d7f4b0cdd264f50bb6d05e6abb7915874a465
Rajibcse8 <rajibtalukder119@gmail.com>
2021-07-29 14:38:37 +0600

    data insert using Query building

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Db;
+
 use App\Models\Category;
 use Auth;
 class CategoryController extends Controller
@@ -30,11 +32,15 @@ class CategoryController extends Controller
         //     'created_at'=>Carbon::now(),
         // ]);
          
-        $category=  new Category;
-        $category->category_name=$request->category_name;
-        $category->user_id=Auth::user()->id;
-        $category->save();
-     
+        // $category=  new Category;
+        // $category->category_name=$request->category_name;
+        // $category->user_id=Auth::user()->id;
+        // $category->save();
+
+        $data=array();
+        $data['category_name']=$request->category_name;
+        $data['user_id']=Auth::user()->id;
+        DB::table('categories')->insert($data);
    
         return Redirect()->back()->with('success','Categorry Added Successfully');
 
