40e9fb0dfb389439ef359983249e09086bfd9e32
Rajibcse8 <rajibtalukder119@gmail.com>
2021-07-31 19:55:47 +0600

    File Uploading Using EORM

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -14,7 +14,33 @@ class BrandController extends Controller
         return view('brand.index');
     }
 
-    public function store(){
-        dd('hello world');
+    public function store(Request  $req){
+       
+
+       $req->validate([
+         'brand_name'=>'required|max:255|unique:brands',
+         'brand_image'=>'required|mimes:jpeg,png,jpg,gif',
+       ],
+       [
+           'brand_name.required'=>'You must have to add a Brand name',
+           'brnad_name.uniqe'=>'We Have Same Brand name Try something Unique',
+           'brand_image.required'=>'you have to pick At least One Picture',
+           'brnad_image.min'=>'Brand longer than 4 character',
+       ]
+    );
+
+     $brand_image=$req->brand_image;
+     $name_gen=hexdec(uniqid());
+     $img_ext=strtolower($brand_image->getClientOriginalExtension());
+     $up_location='image/brand';
+     $img_name=$name_gen.'.'.$img_ext;
+     $brand_image->move($up_location,$img_name);
+
+     Brand::insert([
+         'brand_name'=>$req->brand_name,
+         'user_id'=>Auth::user()->id,
+         'brand_image'=>$img_name,
+     ]);
+      return Redirect()->back()->with('success', 'Brand Data added Successfully');
     }
 }
--- resources/views/brand/index.blade.php
+++ resources/views/brand/index.blade.php
@@ -5,6 +5,14 @@
 
     <div class="py-12">
         <div class="container">
+           @if (session('success'))
+             
+           <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{ session('success') }}</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+           @endif
+
             <div class="row">
                 <div class="col-md-8">
                     <div class="card">
@@ -43,18 +51,24 @@
                             Add-Brand
                         </div>
                         <div class="card-body">
-                            <form action="{{ route('brand.store') }}" method="POST">
+                            <form action="{{ route('brand.store') }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 <div class="form-group">
                                     <label for="Brand_name">Brand Name</label>
                                     <input type="text" class="form-control" name="brand_name" id="Brand_name">
+                                    @error('brand_name')
+                                    <span><strong class="text text-danger">{{ $message }}</strong></span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="Brand_name">Brand iamge</label>
                                     <input type="file" class="form-control" name="brand_image" id="Brand_name">
+                                    @error('brand_image')
+                                         <span><strong class="tect text-danger">{{ $message }}</strong></span>
+                                    @enderror
                                 </div>
 
-                                <input type="submit" class="btn btn-primary" value="ADD Brand">
+                                <button type="submit" class="btn btn-primary">Add Brand</button>
                             </form>
                         </div>
 
